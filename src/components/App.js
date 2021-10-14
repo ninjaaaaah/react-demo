6128e3248cce07d917747ca56801fad7046d0ff3
Jarred Luzada <jarredluzada@gmail.com>
2021-10-14 22:26:19 +0800

    generated profiles

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,44 @@
 import React from "react"
+import {motion} from "framer-motion"
 import Button from './Button'
+
+const hover = { scale: 1.1, backgroundPosition: [`0%`,`100%`]};
 
 export default class App extends React.Component {
     state = {
-        profile: {}
+        profile: null
     }
 
-    generateProfile = async _ => {
+    fetchProfile = async _ => {
         const profile = (await (await fetch('https://randomuser.me/api')).json()).results[0];
-        console.log(profile);
         this.setState({profile});
         console.log(this.state.profile);
     }
 
     render() {
+        this.fetchProfile;
+        console.log(this.state.profile);
         return(
             <>
-                <button id="toggle" onClick={this.generateProfile}>GENERATE</button>
-                <div id="profile"></div>
+                <motion.div 
+                    id="container"
+                    drag="x"
+                    dragConstraints={{ left: 0, right: 0 }}
+                    >
+                    <motion.button
+                        id="toggle"
+                        whileHover={hover}
+                        whileTap={{ scale: 0.9 }}
+                        onClick={this.fetchProfile}
+                    >
+                        GENERATE
+                    </motion.button>
+                </motion.div>
+                <motion.div id="display">
+                    {
+                        this.state.profile && <img src={this.state.profile.picture.large} /> 
+                    }
+                </motion.div>
             </>
         )
     }
